ca6b9a868e001f09454f1cd1da4c0ed0079ffd28
Diego Martins de Siqueira <dieg0@live.com>
2016-05-24 21:30:09 -0300

    First commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Content.PM;
+using Android.Graphics.Drawables;
+
+namespace MaterialDesign.Droid
+{
+	[Activity (Label = "MaterialDesign.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+			FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+	}
+}
+
